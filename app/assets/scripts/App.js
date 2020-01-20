9a481d51b1de822af20728ab006d418086ad473c
Matthias <matthias.fdorf@gmail.com>
2020-01-20 13:05:50 +0100

    chap52 end

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,11 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
+new Modal()
 let stickyheader = new StickyHeader()
+
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 

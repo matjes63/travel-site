7588d0d2907ac2e110dcd84e664d4e296c82a1c9
Matthias <matthias.fdorf@gmail.com>
2020-01-28 08:37:22 +0100

    chap 62 secret code

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,25 @@
-exports.handler = function(event, context, callback)
- {
-     callback(null,  {
-         statusCode: 200,
-         body: "Welcome to the super secret area"
-     })
+exports.handler = function(event, context, callback) {
+    const secretContent = `
+    <h3>Welcome to the secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and anything else</p>
+    `
+    let body
+
+     if (event.body) {
+         body = JSON.parse(event.body)
+     }else {
+         body = {}
+     }
+
+     if (body.password == 'javascript') {
+        callback(null,  {
+            statusCode: 200,
+            body: secretContent
+        })
+
+     } else {
+        callback(null,  {
+            statusCode: 401
+        })
+     }
  }
